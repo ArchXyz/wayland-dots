7b507a4a2769ee15e7a8cc679cb0866941a9016a
ArchXyz <archlinux.xyz@gmail.com>
2023-02-18 10:03:07 +0100

    Add Japaness Font

--- sway-config/swayPkg.sh
+++ sway-config/swayPkg.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed --noconfirm wayland xorg-xwayland sway swaybg swaylock s
 	firefox qutebrowser \
 	nemo mpv imv \
 	arc-gtk-theme papirus-icon-theme \
-	ttf-dejavu ttf-font-awesome ttf-jetbrains-mono-nerd \
+	adobe-source-han-sans-jp-fonts ttf-dejavu ttf-font-awesome ttf-jetbrains-mono-nerd \
 	neovim fzf fd exa bat wl-clipboard trash-cli man-db brightnessctl imagemagick \
 	udiskie dosfstools ntfs-3g \
 	gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-nfs gvfs-gphoto2 \
